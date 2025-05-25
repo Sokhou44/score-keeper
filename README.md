4b0f7503b5ce557c7f863cc05784d3f9d6185d24
Sokhna Gueye <sokhnag98@gmail.com>
2025-05-26 00:54:02 +0200

    commit final

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # score-keeper
+
+Ce projet est une petite application web qui permet de gérer un score entre deux joueurs. Chaque joueur peut augmenter son score jusqu'à atteindre le score maximal défini. Le jeu peut être réinitialisé à tout moment.
+
+# onctionnalités
+Score affiché pour chaque joueur.
+
+Score maximal modifiable via un champ input.
+
+Boutons pour :
+
+Ajouter 1 point à Player 1
+
+Ajouter 1 point à Player 2
+
+Réinitialiser la partie
+
+# Technologies utilisées
+HTML
+
+JavaScript
+
+Sass 
--- script.js
+++ script.js
@@ -1,6 +1,67 @@
-let scoreMax = 7
-let score1 = 0
-let score2 = 0
+"use strict";
 
-const document.querySelector(".scoreMax").textContent = scoreMax
-const document.querySelector('.score')
+const score1Span = document.querySelector(".score1");
+const score2Span = document.querySelector(".score2");
+const scoreInput = document.querySelector(".scoreInput");
+
+const btn1 = document.querySelector(".btnPlayer1");
+const btn2 = document.querySelector(".btnPlayer2");
+const btnReset = document.querySelector(".btnReset");
+
+let score1 = 0;
+let score2 = 0;
+let gameOver = false;
+
+function getMaxScore() {
+  return parseInt(scoreInput.value);
+}
+
+function updateScores() {
+  score1Span.textContent = score1;
+  score2Span.textContent = score2;
+}
+
+function resetGame() {
+  score1 = 0;
+  score2 = 0;
+  gameOver = false;
+  updateScores();
+  score1Span.classList.remove("gagnant", "perdant");
+  score2Span.classList.remove("gagnant", "perdant");
+  
+}
+
+btn1.addEventListener("click", () => {
+   
+    if (gameOver === false) {
+     
+      score1 = score1 + 1;
+  
+      if (score1 === getMaxScore()) {
+        gameOver = true;
+        score1Span.classList.add("gagnant"); 
+        score2Span.classList.add("perdant");  
+      }
+  
+      updateScores();
+    }
+  });
+
+  btn2.addEventListener("click", () => {
+    
+    if (gameOver === false) {
+    
+      score2 = score2 + 1;
+  
+      if (score2 === getMaxScore()) {
+        gameOver = true;
+        score2Span.classList.add("gagnant");
+        score1Span.classList.add("perdant"); 
+      }
+  
+      updateScores();
+    }
+  });
+  
+
+btnReset.addEventListener("click", resetGame);
